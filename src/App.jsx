94cfbff35e1802438ecb26a8f0f5fe034a822c34
eddyionescu <eddyionescu@gmail.com>
2017-11-19 15:10:31 -0800

    Relay initial implementation
    This commit adds middleware using Relay,
    a framework that works with our graphql
    API to give components what they want.
    
    For this to be possible, the component in
    question is wrapped in a QueryRenderer,
    which defines the query that retrieves all
    the data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import DeckGL, { GeoJsonLayer } from 'deck.gl';
+import {
+  QueryRenderer,
+  graphql,
+  // createFragmentContainer,
+} from 'react-relay';
 
 import logo from './res/logo.svg';
 import './styles/App.css';
 import './styles/Zoom.css';
 import { MAPBOX_ACCESS_TOKEN } from './config.json';
 import muniRoutesGeoJson from './res/muniRoutes.geo.json';
-
+import environment from './relayEnv';
 
 const routesLayer = new GeoJsonLayer({
   id: 'muni-routes-geojson',
@@ -42,6 +47,7 @@ class App extends Component {
         minPitch: 0,
         maxPitch: 85,
       },
+      environment,
     };
   }
 
@@ -73,6 +79,56 @@ class App extends Component {
     );
   }
 
+  // container that uses this data is wrapped in a QueryRenderer
+  // TODO wrap map with a FragmentContainer, which declares
+  // all the props it needs to load so that relay can
+  // guarantee that they're available
+  // (as currently it is often not loaded in time)
+  // https://facebook.github.io/relay/docs/fragment-container.html
+  renderMapRelay() {
+    // Render this somewhere with React:
+    return (
+      <QueryRenderer
+        environment={this.state.environment}
+        query={graphql`
+          query AppAllVehiclesQuery($agency: String!, $startTime: String!) {
+            trynState(agency: $agency, startTime: $startTime) {
+              agency
+              startTime
+              states {
+                time
+                routes {
+                  name
+                  vehicles {
+                    vid
+                    lat
+                    lon
+                    heading
+                  }
+                }
+              }
+            }
+          }
+        `}
+        variables={{
+          agency: 'muni',
+          startTime: Date.now(),
+        }}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>{error.message}</div>;
+          } else if (props) {
+            const { trynState } = props;
+            return trynState.states[0].routes.map(route => (
+              <div>{route.name} - {route.vehicles.length} vehicles</div>
+            ));
+          }
+          return <div>Loading</div>;
+        }}
+      />
+    );
+  }
+
   render() {
     return (
       <div>
@@ -85,6 +141,7 @@ class App extends Component {
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
         </div>
+        {this.renderMapRelay()}
         {this.renderMap()}
       </div>
     );
