8d1e6ce634cd1ea5b3a2c436d61be973a3fe5209
Caleb Rogers <rogersjcaleb@gmail.com>
2018-11-07 19:12:56 -0800

    Remove buggy tool
    Throwing hundreds of websocket errors a minute

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { installRelayDevTools } from 'relay-devtools';
 import './styles/index.css';
 import App from './App';
 
-installRelayDevTools();
 
 ReactDOM.render(
   <App />,
