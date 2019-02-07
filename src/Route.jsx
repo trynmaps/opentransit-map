02493ff67e6224f1175fe655ad19d440933b2768
Yi Liu <fruit13ok@gmail.com>
2019-02-06 20:07:24 -0800

    done line icon colors (#94)
    * done line icon colors
    
    * fixed merge
    
    * Delete yarn.lock
    
    * remove console log
    
    * remove build/favicon.ico
    
    * fixed minor issues

--- src/Route.jsx
+++ src/Route.jsx
@@ -13,9 +13,26 @@ const STOP_ICON_MAPPING = {
 
 const BUS_ICON_MAPPING = {
   marker: {
-    x: 0, y: 0, width: 128, height: 128, mask: false,
+    x: 0, y: 0, width: 128, height: 128, mask: true,
   },
 };
+
+/* https://en.wikipedia.org/wiki/Template:MUNI_color */
+const MUNI_COLOR = [
+  { line: 'E', color: [102, 102, 102] },
+  { line: 'F', color: [240, 230, 140] },
+  { line: 'J', color: [250, 166, 52] },
+  { line: 'KT', color: [86, 155, 190] },
+  { line: 'L', color: [146, 39, 143] },
+  { line: 'M', color: [0, 135, 82] },
+  { line: 'N', color: [0, 83, 155] },
+  { line: 'S', color: [255, 204, 0] },
+  // { line: 'T', color: [211, 18, 69] },
+  { line: 'PM', color: [83, 161, 177] },
+  { line: 'PH', color: [71, 176, 153] },
+  { line: 'C', color: [116, 167, 178] },
+  { line: 'default', color: [204, 0, 51] },
+];
 
 /* changes color of selected stop along a route */
 function selectedStopColor(stop, selectedStops) {
@@ -75,7 +92,29 @@ export function getSubRoutesLayer(subroute) {
   return subrouteLayer;
 }
 
+/* convert line name to color */
+function lineToColor(line) {
+  const checkedLine = MUNI_COLOR.filter(e => e.line === line);
+  if (checkedLine.length > 0) {
+    return checkedLine[0].color;
+  }
+  return MUNI_COLOR[MUNI_COLOR.length - 1].color;
+}
+
 export function getVehicleMarkersLayer(route, displayVehicleInfo) {
+  /* return a new IconLayer */
+  function newIconLayer(id, data, iconAtlas) {
+    return new IconLayer({
+      id,
+      data,
+      iconAtlas,
+      iconMapping: BUS_ICON_MAPPING,
+      pickable: true,
+      // calls pop-up function
+      onClick: info => displayVehicleInfo(info),
+    });
+  }
+
   /* returns new DeckGL Icon Layer displaying all vehicles on given routes */
   const westData = route.routeStates[0].vehicles.reduce((westBus, vehicle) => {
     if (vehicle.heading >= 180) {
@@ -84,7 +123,7 @@ export function getVehicleMarkersLayer(route, displayVehicleInfo) {
         icon: 'marker',
         size: 128,
         angle: 270 - vehicle.heading,
-        color: [0, 0, 255],
+        color: lineToColor(route.rid),
         // added vid & heading info to display onClick pop-up
         vid: vehicle.vid,
         heading: vehicle.heading,
@@ -100,7 +139,7 @@ export function getVehicleMarkersLayer(route, displayVehicleInfo) {
         icon: 'marker',
         size: 128,
         angle: 90 - vehicle.heading,
-        color: [0, 0, 255],
+        color: lineToColor(route.rid),
         // added vid & heading info to display onClick pop-up
         vid: vehicle.vid,
         heading: vehicle.heading,
@@ -110,23 +149,7 @@ export function getVehicleMarkersLayer(route, displayVehicleInfo) {
   }, []);
 
   return [
-    new IconLayer({
-      id: 'west-vehicle-icon-layer',
-      data: westData,
-      iconAtlas: busIconWest,
-      iconMapping: BUS_ICON_MAPPING,
-      pickable: true,
-      // calls pop-up function
-      onClick: info => displayVehicleInfo(info),
-    }),
-    new IconLayer({
-      id: 'east-vehicle-icon-layer',
-      data: eastData,
-      iconAtlas: busIconEast,
-      iconMapping: BUS_ICON_MAPPING,
-      pickable: true,
-      // calls pop-up function
-      onClick: info => displayVehicleInfo(info),
-    }),
+    newIconLayer('west-vehicle-icon-layer', westData, busIconWest),
+    newIconLayer('east-vehicle-icon-layer', eastData, busIconEast),
   ];
 }
